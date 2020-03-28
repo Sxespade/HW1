ab7c2c977297f1129fe1c982058076b83eea795c
Sxespade <47774531+Sxespade@users.noreply.github.com>
2020-03-28 20:59:56 +0300

    Delete MyArrayDataException.java

--- src/HomeWork2/MyArrayDataException.java
+++
@@ -1,8 +0,0 @@
-package HomeWork2;
-
-public class MyArrayDataException extends RuntimeException {
-
-    public MyArrayDataException(int i, int j) {
-        super("В ячейке [" + i + "][" + j + "] лежит не число!");
-    }
-}
