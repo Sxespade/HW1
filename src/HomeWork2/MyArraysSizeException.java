562d5660b92c36b927ad9da41dbafca48a35995d
Sxespade <47774531+Sxespade@users.noreply.github.com>
2020-03-28 21:00:04 +0300

    Delete MyArraysSizeException.java

--- src/HomeWork2/MyArraysSizeException.java
+++
@@ -1,8 +0,0 @@
-package HomeWork2;
-
-public class MyArraysSizeException extends RuntimeException {
-
-    public MyArraysSizeException() {
-        super("Передан массив неправильных размеров!");
-    }
-}
